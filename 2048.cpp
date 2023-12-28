c9392b42b583f324a606be532ac314a2b82551e5
Rodyll Jan Eata <rjeata@gmail.com>
2023-12-28 16:58:26 +1000

    Added more helper function and fixed bugs

--- 2048.cpp
+++ 2048.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <conio.h>
 #include <iomanip>
+#include <cstring>
 
 #define KEY_UP 72
 #define KEY_DOWN 80
@@ -12,15 +13,20 @@
 
 bool processInput(std::string);
 void playGame();
-bool moveArray(int[4][4], int);
+bool moveArray(int[4][4], int, int &);
 void displayArray(int[4][4]);
 bool checkValidMoves(int[4][4]);
 bool isFull(int[4][4]);
+bool getScore(int[4][4]);
 
 int main(){
     std::string input;
 
-    std::cout << "Welcome to 2048" << std::endl;
+    std::cout << "|\033[0;43m              \033[0m|\n";
+    std::cout << "|\033[0;43m              \033[0m|\n";
+    std::cout << "|\033[0;43;33m  2  0  4  8  \033[0m|\n";
+    std::cout << "|\033[0;43m              \033[0m|\n";
+    std::cout << "|\033[0;43m              \033[0m|\n";
 
     do{
         std::cout << "1     -   Start Game\n";
@@ -30,10 +36,12 @@ int main(){
         std::cin >> input;
     }while(processInput(input));
 
-    if(input == "1")
+    if(input == "1"){
         playGame();
-
-    std::cout << "Thank you for playing, Goodbye\n";
+        std::cout << "Thank you for playing, Goodbye\n";
+    }
+    else    
+        std::cout << "Exiting game, Goodbye\n";
 
     return 0;
 }
@@ -41,12 +49,11 @@ int main(){
 bool processInput(std::string input){
     if(input == "1")
         return false;
-    else if(input == "2")
+    else if(input == "2"){
         std::cout << "How to Play\n";
-    else if(input == "3" or input == "q"){
-        std::cout << "Quitting\n";
-        return false;
     }
+    else if(input == "3" or input == "q")
+        return false;
     else
         std::cout << "Invalid Input\n";
     
@@ -59,6 +66,8 @@ void playGame(){
     int randX = 0;
     int randY = 0;
     int input = 0;
+    int score = 0;
+    bool win = false;
     
     srand(time(NULL));
 
@@ -70,40 +79,55 @@ void playGame(){
         //  clear consle and print array
         system("cls");
         displayArray(array);
+        std::cout << "Score = " << score << std::endl;
 
         std::cout << "Waiting for move. (q - Exit game) \n";
         input = getch();
 
         //  if input is valid, move array
-        if(moveArray(array, input)){
+        if(moveArray(array, input, score)){
             system("cls");
             displayArray(array);
 
+            if(!win and getScore(array)){
+                std::cout << "Congradulations, you win!\n";
+                std::cout << "Quit game? (q)\nPress any input to continue game.\n";
+
+                input = getch();
+                if(input == QUIT)  
+                    break;
+                else 
+                    win = true;
+            }
+
             //  check if there are any valid moves left, if so, populate a new tile
             if(checkValidMoves(array) and !isFull(array)){
-                
-                /*
-                    TODO:
-                        -   figure out how to add two tiles at once
-                        -   figure out how to add 4 instead of 2 sometimes
-                */
+                randX = rand() % 4;
+                randY = rand() % 4;
+
                 while(array[randX][randY] != 0){
                     randX = rand() % 4;
                     randY = rand() % 4;
                 }
-            
-                //  set tile equal to 2
-                array[randX][randY] = 2;
-            }
+                
+                int r = rand() % 10;
 
-            //  else if there are no valid moves left, end game
-            else if(!checkValidMoves(array)){
-                std::cout << "Game Over\n";
-                break;
+                //  set new tile to 2 if random number is less than 9, else set to 4
+                if(r < 9)
+                    array[randX][randY] = 2;
+                else 
+                    array[randX][randY] = 4;
             }
         }
 
-        //  else if input is quit, ask for confirmation
+        //  else end game if there are no valid moves left
+        else if(!checkValidMoves(array)){
+            std::cout << "Score = " << score << std::endl;
+            std::cout << "Game Over\n";
+            break;
+        }
+
+        //  else if input is quit, ask for confirmation then quit
         else if(input == QUIT){
             std::cout << "Quit game? (q)\nPress any input to cancel.\n";
 
@@ -114,10 +138,11 @@ void playGame(){
     } 
 }
 
-bool moveArray(int array[4][4], int move){
+bool moveArray(int array[4][4], int move, int & score){
     int lastMerge = 0;
     int prevPos = 0;
     int nextPos = 0;
+    bool swapped = false;
 
     if(move == KEY_UP){
         for(int j = 0; j < 4; j++){
@@ -135,26 +160,33 @@ bool moveArray(int array[4][4], int move){
                     //  move to empty tile
                     if(array[prevPos][j] == 0){
                         array[prevPos][j] = array[i][j];
+                        swapped = true;
                     }
                     
                     //  combine matching tiles
                     else if(array[prevPos][j] == array[i][j]){
                         array[prevPos][j] *= 2;
+                        array[i][j] = 0;
+
+                        score += array[prevPos][j];
+                        
                         lastMerge = prevPos + 1;
+                        swapped = true;
                     }
                     
                     //  move to next tile
                     else if(array[prevPos][j] != array[i][j] and prevPos + 1 != i){
                         array[prevPos + 1][j] = array[i][j];
+                        swapped = true;
                     }
+
                     if(prevPos + 1 != i or array[i][j] == array[prevPos][j]){
-                        array[i][j] = 0;
+                        array[i][j] = 0; 
+                        swapped = true;
                     }
                 }
             }
         }
-
-        return true;
     }
     else if(move == KEY_DOWN){
         for(int j = 0; j < 4; j++){
@@ -172,28 +204,33 @@ bool moveArray(int array[4][4], int move){
                     //  move to empty tile
                     if(array[nextPos][j] == 0){ 
                         array[nextPos][j] = array[i][j];
+                        swapped = true;
                     }
 
                     //  merge matching tiles
                     else if(array[nextPos][j] == array[i][j]){
                         array[nextPos][j] *= 2;
-                     lastMerge = nextPos - 1;
+                        array[i][j] = 0;
+                        lastMerge = nextPos - 1;
+                        swapped = true;
+
+                        score += array[nextPos][j];
                     }
 
                     //  move to next tile
                     else if(array[nextPos][j] != array[i][j] and nextPos - 1 != i){ 
+                        array[nextPos - 1][j] = array[i][j];
+                        swapped = true;
                     }
 
                     if(nextPos - 1 != i or array[i][j] == array[nextPos][j]){
+                        swapped = true;
                         array[i][j] = 0;
                     }
                 }
             }
         }
-
-        return true;
     }
-
     else if(move == KEY_LEFT){
         for(int i = 0; i < 4; i++){
             lastMerge = 0;
@@ -210,28 +247,33 @@ bool moveArray(int array[4][4], int move){
                     //  move to empty tile
                     if(array[i][prevPos] == 0){
                         array[i][prevPos] = array[i][j];
+                        swapped = true;
                     }
 
                     //  combine matching tile
                     else if(array[i][prevPos] == array[i][j]){
                         array[i][prevPos] *= 2;
+                        array[i][j] = 0;
                         lastMerge = prevPos + 1;
+                        swapped = true;
+
+                        score += array[i][prevPos];
                     }
 
                     //  move to next position
                     else if(array[i][prevPos] != array[i][j] and prevPos + 1 != j){
                         array[i][prevPos + 1] = array[i][j];
+                        swapped = true;
                     }
 
                     if(prevPos + 1 != j or array[i][j] == array[i][prevPos]){
                         array[i][j] = 0;
+                        swapped = true;
                     }
                 }
             }
         }
-        return true;
     }
-
     else if(move == KEY_RIGHT){
         for(int i = 0; i < 4; i++){
             lastMerge = 3;
@@ -248,37 +290,44 @@ bool moveArray(int array[4][4], int move){
                     //  move to empty tile
                     if(array[i][nextPos] == 0){
                         array[i][nextPos] = array[i][j];
+                        swapped = true;
                     }
 
                     //  combine matching tile
                     else if(array[i][nextPos] == array[i][j]){
                         array[i][nextPos] *= 2;
+                        array[i][j] = 0;
                         lastMerge = nextPos - 1;
+                        swapped = true;
+
+                        score += array[i][nextPos];
                     }
 
                     //  move to next position
                     else if(array[i][nextPos] != array[i][j] and nextPos - 1 != j){
                         array[i][nextPos - 1] = array[i][j];
+                        swapped = true;
                     }
 
                     if(nextPos - 1 != j or array[i][j] == array[i][nextPos]){
                         array[i][j] = 0;
+                        swapped = true;
                     }
                 }
             }
         }
-
-        return true;
     }
 
-    return false;
+    return swapped;
 }
 
 bool checkValidMoves(int array[4][4]){
     for(int i = 0; i < 4; i++){
         for(int j = 0; j < 4; j++){
-            //  check above, below, left, and right for valid moves, 
-            //      return true if there are any
+            //  check if tile is empty, alo check above, below, left, and right for 
+            //      valid moves, return true if there are any
+            if(array[i][j] == 0)
+                return true;
             if(i > 0 and array[i][j] == array[i-1][j]) 
                 return true; 
             if(i < 3 and array[i][j] == array[i+1][j]) 
@@ -306,15 +355,44 @@ bool isFull(int array[4][4]){
 }
 
 void displayArray(int array[4][4]){
-    std::cout << "-------------------------------------\n";
+    std::cout << "------------------------------------------\n";
 
     for(int i = 0; i < 4; i++){
         std::cout << "|";
+
         for(int j = 0; j < 4; j++){
-            std::cout << std::setfill(' ') << std::setw(5) 
-                << array[i][j] << "   |";
+            switch(array[i][j]){
+                case 2:     std::cout << "\033[0;47;33m"; break; // white background yellow text
+                case 4:     std::cout << "\033[0;42;31m"; break; // green background red text
+                case 8:     std::cout << "\033[0;43;32m"; break; // yellow background green text
+                case 16:    std::cout << "\033[0;44;34m"; break; // blue background blue text
+                case 32:    std::cout << "\033[0;45;32m"; break; // purple background green text
+                case 64:    std::cout << "\033[0;46;33m"; break; // cyan background yellow text
+                case 128:   std::cout << "\033[0;42;34m"; break; // green background blue text
+                case 256:   std::cout << "\033[0;102;33m"; break; // high green background yellow text
+                case 512:   std::cout << "\033[0;103;31m"; break; // high yellow background red text
+                case 1024:  std::cout << "\033[0;106;33m"; break; // high cyan background yellow text
+                case 2048:  std::cout << "\033[0;41;33m"; break; // red background yellow text
+                // Add more cases if needed
+                default:    std::cout << "\033[0m";  break; // Reset
+            }
+            std::cout << std::setfill(' ') << std::setw(5) << array[i][j] << "    \033[0m "; //  |";
         }
-        std::cout << "\n-------------------------------------\n";
+        std::cout << "|\n------------------------------------------\n";
 
     }
+
+}
+
+bool getScore(int array[4][4]){
+    int sum = 0;
+
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            if(array[i][j] == 2048)
+                return true;
+        }
+    }
+
+    return false;
 }
